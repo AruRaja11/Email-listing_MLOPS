2fe786b4abb74b971abf16c8d7fa505222ad25db
Arun Raja <147836952+AruRaja11@users.noreply.github.com>
2026-02-13 11:32:49 +0530

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,5 +2,8 @@ from pipelines.training_pipeline import training_line
 from zenml.client import Client
 
 if __name__ == "__main__":
-    print(Client().active_stack.experiment_tracker.get_tracking_uri())
+    if Client().active_stack.experiment_tracker:
+        print(Client().active_stack.experiment_tracker.get_tracking_uri())
+    else:
+        print("No experiment tracker found in the active stack.")
     training_line("data/email_listing.csv")
